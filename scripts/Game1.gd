8eddac34d776d2cb709cbdb4c7a2e275895d29ac
xiaoxuan <hengxiaoxuan2003@gmail.com>
2025-05-24 21:22:05 +0800

    Entry Dialogue for Game1

--- scripts/Game1.gd
+++ scripts/Game1.gd
@@ -4,11 +4,8 @@ extends Node2D
 @onready var player = $Monyet 
 
 var intro_dialogue = [
-	{"name": "Monyet", "text": "Another year, still just… a monyet."},
-	{"name": "Monyet", "text": "I've watched so many students walk these grounds... real UM students."},
-	{"name": "Monyet", "text": "But this time, I’m ready."},
-	{"name": "Monyet", "text": "To become one of them, I must face every challenge... and pass."},
-	{"name": "Monyet", "text": "Let’s do this. My journey to graduate begins—now!"}
+	{"name": "Monyet", "text": "Alright… grab the keys, dodge the lecturers, find the exit."},
+	{"name": "Monyet", "text": "Simple enough… right?"}
 ]
 
 func _ready():
